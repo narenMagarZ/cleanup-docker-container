3a469a45fb4e8d94ed5e8849b8c05aafdb54932d
naren <nagendra.magar@varosatech.com>
2026-02-12 08:04:06 +0545

    add script to detect containers along with volumes greather than 30 days and just log id and names for now

--- script.sh
+++ script.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-fetch_containers () {
-    
-}
+# current date in sec
+current_date=$(date +%s)
+
+WINDOW_DAYS=30
+
+is_dead_container_detected=0
+
+while read -r id name; do
+
+    container_created_date=$(docker inspect "$id" -f "{{.Created}}")
+
+    # container_created_date in sec
+    trimmed_date=$(date -j -f '%Y-%m-%dT%H:%M:%S' "$container_created_date%%.*" +%s)
+
+    # estimate days
+    date_in_days=$(( (current_date - trimmed_date) / 86400 ))
+
+    if [ "$date_in_days" -gt "$WINDOW_DAYS" ]; then
+        is_dead_container_detected=1
+        echo "Removing container $id ($name) with volumes"
+        # docker rm -v "$id"
+    fi
+done < <(docker ps -a -f status=exited -f status=dead --format "{{.ID}} {{.Names}}")
+
+if [ "$is_dead_container_detected" -eq 0 ] ; then
+    echo "Nothing to remove!"
+fi
